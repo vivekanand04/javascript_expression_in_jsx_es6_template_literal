a1cbbab370744098a67aeb73c64a4c4a90f94ab0
vivekanand04 <126641185+vivekanand04@users.noreply.github.com>
2023-07-05 10:12:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// QUESTIO IS WRITTEN IN COPPY ,,USKA CODE YAHA HAI
+
+import React from "react";
+import ReactDOM from "react-dom";
+const name = "vivek"; //it is js code
+const num = 7; //it is js code
+ReactDOM.render(
+  <div>
+    <h1>My name is {name}</h1>
+    <p> my lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
+{
+  /*...... kuchh karo......
+RENDER KE ANER JO NUM HSI USke jagah  LIKHO 
+ 1)3+4 then answer will be same 
+2)Math.floor(maths.random()*10)*/
+}
